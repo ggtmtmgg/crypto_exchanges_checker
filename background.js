88d7ef308088f020c6280cc967f49b612c2be13d
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-01-14 00:03:04 +0900

    feat check url by it's hostname

--- background.js
+++ background.js
@@ -1,9 +1,27 @@
-var blacklist = [''];
+// var blacklist = [''];
+var blacklist = ['www.binance.com']; // TODO: replace real blacklist
+
+function hostname(url) {
+  var hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+  if (url.indexOf("://") > -1) {
+      hostname = url.split('/')[2];
+  }
+  else {
+      hostname = url.split('/')[0];
+  }
+  //find & remove port number
+  hostname = hostname.split(':')[0];
+  //find & remove "?"
+  hostname = hostname.split('?')[0];
+  return hostname;
+}
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  if(blacklist.includes(tab.url)){
+  var tab_url_host = hostname(tab.url);
+  if(blacklist.includes(tab_url_host)){
     if (changeInfo.status == 'complete') {
-      alert('このURLはブラックリストに登録されています。注意してください。');
+      alert(tab_url_host + 'はブラックリストに登録されています。注意してください。');
     }
   }
 })
